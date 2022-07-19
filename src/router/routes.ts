7a9d9c5c69694e08b915d0399533c3c740d9f6d0
timothy okooboh <okoobohtimothy@gmail.com>
2022-07-19 17:28:27 +0100

    create route for resume builder

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,12 @@
 export default [
     {
         path: '/',
+        name: 'Home',
         component: () => import("../views/Home.vue")
+    },
+    {
+        path: '/create-resume/:stage/:resume_id',
+        name: 'ResumeBuilder',
+        component: () => import('../views/ResumeBuilder.vue')
     }
 ]
